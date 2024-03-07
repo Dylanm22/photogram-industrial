38dde62f8548e8a81b735e24d25a183291bbfdf5
Dylan Menard <148174649+Dylanm22@users.noreply.github.com>
2024-03-07 17:12:45 +0000

    fixed my sample data

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  root "photos#index"
+  
   resources :likes
   resources :follow_requests
   resources :comments
@@ -7,5 +10,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "photos#index"
+
 end
